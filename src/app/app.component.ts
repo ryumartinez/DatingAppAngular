c42dd70ae579377689ad61bd8ad58182a23c0ca0
ryumartinez <ryueduardomartinez@outlook.es>
2023-05-14 17:28:25 -0400

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = "Dating app";
-  users:any;
+export class AppComponent implements OnInit {
+  title = 'Dating app';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.http.get("https://localhost:44363/api/Users").subscribe({
-      next: response => this.users = response,
-      error: (error) => console.log(error),
-      complete: () => console.log("request has completed")
-    });
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
 
 }
