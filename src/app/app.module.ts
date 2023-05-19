5669aea64be6dad219f77f431dde00e2b84b5f35
ryumartinez <ryueduardomartinez@outlook.es>
2023-05-19 06:46:53 -0400

    crud photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
-import {NgxSpinnerModule} from "ngx-spinner";
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +38,8 @@ import {NgxSpinnerModule} from "ngx-spinner";
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -46,8 +47,7 @@ import {NgxSpinnerModule} from "ngx-spinner";
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    SharedModule,
-    NgxSpinnerModule
+    SharedModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
